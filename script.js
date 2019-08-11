b4314c65a81a423d4ae8823903070d0715c9f331
Andrew Litt <andrew.litt@queensu.ca>
2019-08-11 15:20:39 -0400

    Site updated: 2019-08-11 15:20:39

---
+++ script.js
@@ -0,0 +1,40 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w|'|,)/g, "<span class='letters'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.intro-card',
+    opacity: 1,
+    easing:'easeInQuad',
+    duration: 300
+  }).add({
+    targets: '',
+    duration: 200
+  }).add({
+    targets: '.ml10 .letters',
+    rotateY: [-90, 0],
+    duration: 1000,
+    delay: function(el, i) {
+      return 40 * i;
+    }
+  }).add({
+    targets: '.ml11',
+    opacity: 1,
+    easing:'easeInOutSine',
+    duration: 500
+  }).add({
+    targets: '.ml12',
+    opacity: 1,
+    easing:'easeInOutSine',
+    duration: 500
+  });
+
+  anime({
+    targets: '.arrow',
+    translateY: 10,
+    scale: 1.2,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine'
+  });
